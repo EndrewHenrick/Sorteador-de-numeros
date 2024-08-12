9beb73e9f3ff0ae726f3746c50c83bd05f47041a
Endrewhenrick <endrewhenrick11@gmail.com>
2024-08-12 17:17:15 -0300

    Jogo Completo 1.0

--- app.js
+++ app.js
@@ -6,14 +6,56 @@ function sortear() {
     let sorteados = [];
     let numero;
 
+    if (de > ate) {
+        alert('Dados inválidos: verifique se os dados estão corretos.')
+        limparCampo();
+        return;
+    }
+
+    if (quantidade > (ate - de + 1)) {
+        alert('Erro: valor no campo "quantidade" inválida, verifique se o dado está correto.')
+        return;
+    }
+
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero); //serve para não repetir o número.
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>` //altera a mensagem na tela
+    alterarStatusBotao();
+
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.remove('container__botao-desabilitado');
+    }
+}
+
+function limparCampo() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+}
+
+function reiniciar() {
+    limparCampo();
+    alterarStatusBotao();
 }
